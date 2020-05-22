ff845873fd349ec171cf409b1ca96d912dfbc778
dpusnyj <dpcarnage86@gmail.com>
2020-05-22 16:14:30 +0300

    a

--- normalizer.js
+++ normalizer.js
@@ -10,15 +10,18 @@ app.post('/normalizer', jsonParser, function (req, res) {
     console.log(req.body);
     const { countryCode, number } = req.body;
     let data = [];
-    for (let i = 0; i < countryCode.length; i++) { 
-        const phoneNo = parseNumber.parsePhoneNumberFromString(number[i], countryCode[i]);
-        if (phoneNo && phoneNo.isValid()) {
-            data.push(phoneNo.number);
-        }
-        else {
-            data.push('Invalid phone number');
-        }
+    try {
+        for (let i = 0; i < countryCode.length; i++) { 
+            const phoneNo = parseNumber.parsePhoneNumberFromString(number[i], countryCode[i]);
+            if (phoneNo && phoneNo.isValid()) {
+                data.push(phoneNo.number);
+            }
+            else {
+                data.push('Invalid phone number');
+            }
+        }  
     }
+    catch(e) {console.log(e)};
     res.send(data);
 })
  
