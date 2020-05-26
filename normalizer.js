2d0a5dad4321e0d2cf2845e5e311705d163c8458
dpusnyj <dpcarnage86@gmail.com>
2020-05-26 14:03:36 +0300

    Add field

--- normalizer.js
+++ normalizer.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
 const parseNumber = require('libphonenumber-js');
+const accountSid = 'AC8cb8e1d5b9cbe3cb3c48fe2ab8803597';
+const authToken = '6fffebd4e1b8e2abf4ee020fd46ca08c';
+const client = require('twilio')(accountSid, authToken);
 const port = process.env.PORT;
 
 var app = express();
@@ -13,9 +16,15 @@ app.post('/normalizer', jsonParser, function (req, res) {
         for (let i = 0; i < data.length; i++) {
             if (!data[i].telephone || !data[i].countryCode) { data[i].normalised = "Invalid Phone" }
             else {
+                data[i].carrier = "";
                 data[i].normalised = data[i].telephone.replace(/[^0-9]/gim,'');
                 const number = parseNumber.parsePhoneNumberFromString(data[i].normalised, data[i].countryCode);
-                if (number && number.isValid()) { data[i].normalised = number.number; }
+                if (number && number.isValid()) {
+                    data[i].normalised = number.number;
+                    client.lookups.phoneNumbers(data[i].normalised)
+                        .fetch()
+                        .then(phone_number => {if (phone_number.carrier.type === "mobile" && phone_number.carrier.name) data[i].carrier = "Type: " + phone_number.carrier.type + " Carrier: " + phone_number.carrier.name;});
+                }
                 else { data[i].normalised = "Invalid Phone" };
             }
         }
